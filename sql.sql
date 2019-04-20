771f46b24886d4cb40012cf10314742b52bd622a
Gabriel Soares <gabriel77soares77@gmail.com>
2019-04-20 10:17:25 -0300

    sql

---
+++ sql.sql
@@ -0,0 +1,41 @@
+drop DATABASE loja;
+CREATE DATABASE loja;
+USE loja;
+
+CREATE TABLE adms (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(120),
+    cpf INT UNIQUE KEY,
+    email VARCHAR(120) UNIQUE KEY,
+    username VARCHAR(120) UNIQUE KEY,
+    password VARCHAR(120)
+);
+
+CREATE TABLE produtos (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(120),
+    descricao TEXT,
+    imagem_url VARCHAR(300),
+    preco DECIMAL(7,2),
+    quantidade INT,
+    categoria INT,
+    id_adm INT,
+    FOREIGN KEY (id_adm) REFERENCES adms(id)
+);
+
+CREATE TABLE clientes (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(120),
+    cpf INT UNIQUE KEY,
+    email VARCHAR(120) UNIQUE KEY,
+    username VARCHAR(120) UNIQUE KEY,
+    password VARCHAR(120)
+);
+
+CREATE TABLE compras (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_cliente INT,
+    id_produto INT,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id)
+);
